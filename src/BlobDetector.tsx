2c5787500147ebe26dd425e80f0640a56bb1b212
hphilley15 <58454685+hphilley15@users.noreply.github.com>
2023-09-23 15:17:34 +0800

    Add Canny Edge Detector

--- src/BlobDetector.tsx
+++ src/BlobDetector.tsx
@@ -2,14 +2,35 @@ import React from 'react';
 import cv from '@techstark/opencv-js';
 
 class BlobDetector extends React.Component {
+  inputImgRef: React.RefObject<HTMLCanvasElement>;
+  edgeImgRef: React.RefObject<HTMLCanvasElement>;
+
   state: { imgUrl: string | null };
 
   constructor(props: {}) {
     super(props);
+    this.inputImgRef = React.createRef();
+    this.edgeImgRef = React.createRef();
     this.state = {
       imgUrl: null,
     };
   }
+
+  async cannyEdgeDetector(imgSrc: HTMLImageElement) {
+    console.log('Running Canny Edge Detector');
+    const img = cv.imread(imgSrc);
+    const grayImg = new cv.Mat();
+    cv.cvtColor(img, grayImg, cv.COLOR_BGR2GRAY);
+    const edgeImg = new cv.Mat();
+    cv.Canny(grayImg, edgeImg, 80, 160);
+
+    cv.imshow(this.edgeImgRef.current!, edgeImg);
+
+    img.delete();
+    grayImg.delete();
+    edgeImg.delete();
+  }
+
   render() {
     const { imgUrl } = this.state;
 
@@ -30,8 +51,18 @@ class BlobDetector extends React.Component {
           />
         </div>
         {imgUrl && (
-          <div className="imageCanvas">
-            <img src={imgUrl} />
+          <div>
+            <div className="imageCanvas">
+              <img
+                src={imgUrl}
+                onLoad={(e: React.ChangeEvent<HTMLImageElement>) => {
+                  this.cannyEdgeDetector(e.target!);
+                }}
+              />
+            </div>
+
+            <div className="imageCanvas"></div>
+            <canvas ref={this.edgeImgRef} />
           </div>
         )}
       </div>
